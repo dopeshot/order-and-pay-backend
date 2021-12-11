929654e3a512a833504e93ca37583b2d37ada6f7
coffemakingtoaster <mh340@hdm-stuttgart.de>
2021-12-11 14:09:47 +0100

    testing

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -3,13 +3,15 @@ import { MailService } from './mail.service';
 
 @Controller('mail')
 export class MailController {
-  constructor(private readonly mailService: MailService) {}
+    constructor(private readonly mailService: MailService) {}
 
-  // This is an endpoint sorely meant to be used for testing purposes. 
-  // Disable this once done testing or delete the controller enturely
-  @Post()
-  async sendMail(@Body('recipient') recipient: string) {
-    return await this.mailService.mailTest(recipient, "this is a dummy endpoint");
-  }
-
+    // This is an endpoint sorely meant to be used for testing purposes.
+    // Disable this once done testing or delete the controller enturely
+    @Post()
+    async sendMail(@Body('recipient') recipient: string) {
+        return await this.mailService.mailTest(
+            recipient,
+            'this is a dummy endpoint'
+        );
+    }
 }
