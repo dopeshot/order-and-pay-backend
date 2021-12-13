965e36c45d3a50c15fd3d7a4c674155360f81522
coffemakingtoaster <mh340@hdm-stuttgart.de>
2021-12-13 14:49:09 +0100

    add best practice rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,20 @@ module.exports = {
   },
   plugins: [
     '@typescript-eslint/eslint-plugin',
-    'prettier',],
+    'prettier',
+    "eslint-comments",
+    "promise",
+    "unicorn",
+    "import"
+  ],
   extends: [
-    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'airbnb-typescript/base',
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:eslint-comments/recommended",
+    "plugin:promise/recommended",
+    "plugin:unicorn/recommended",
+    "prettier"
   ],
   root: true,
   env: {
@@ -18,23 +28,42 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/ban-types': 'error',
-    '@typescript-eslint/no-empty-interface': ["error", {
-      "allowSingleExtends": true, // allow extending of a single interface"
-    }],
-    "@typescript-eslint/no-this-alias": [
+   // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
+    "no-prototype-builtins": "off",
+    // https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
+    "import/prefer-default-export": "off",
+    "import/no-default-export": "error",
+    // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
+    "react/destructuring-assignment": "off",
+    // No jsx extension: https://github.com/facebook/create-react-app/issues/87#issuecomment-234627904
+    "react/jsx-filename-extension": "off",
+    // Use function hoisting to improve code readability
+    "no-use-before-define": [
       "error",
-      {
-        "allowDestructuring": false, // Disallow `const { props, state } = this`; true by default
-        "allowedNames": ["self"] // Allow `const self = this`; `[]` by default
-      }
+      { functions: false, classes: true, variables: true },
     ],
-    '@typescript-eslint/no-unnecessary-type-assertion': ["warn"],
-    '@typescript-eslint/consistent-type-imports': 'error',
-    "@typescript-eslint/ban-ts-comment": ["warn"] // Warn for ts-ignore and every other ts-comment
+    // Allow most functions to rely on type inference. If the function is exported, then `@typescript-eslint/explicit-module-boundary-types` will ensure it's typed.
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-use-before-define": [
+      "error",
+      { functions: false, classes: true, variables: true, typedefs: true },
+    ],
+    // Common abbreviations are known and readable
+    "unicorn/prevent-abbreviations": "off",
+    // Airbnb prefers forEach
+    "unicorn/no-array-for-each": "off",
+    'unicorn/no-abusive-eslint-disable': 'off',
+    // It's not accurate in the monorepo style
+    "import/no-extraneous-dependencies": "off",
+    "eslint-comments/disable-enable-pair": ["error", {"allowWholeFile": true}],
   },
+  overrides: [
+    {
+      files: ["*.js"],
+      rules: {
+        // Allow `require()`
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    }
+  ],
 };
