07effb37e92b60ddf3a56172acbd1e4f2e19afd2
coffemakingtoaster <mh340@hdm-stuttgart.de>
2021-12-16 16:56:01 +0100

    resolve conflict between prettier and eslint

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,4 +5,5 @@ module.exports = {
   "printWidth": 80, // Limit line length
   "tabWidth": 4, // I don´t like people using 2 spaces for tabs
   "bracketSpacing": true, // Add spaces around brackets
+  "endOfLine":"auto", // Use the OS line ending
 };
