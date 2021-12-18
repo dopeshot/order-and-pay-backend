dd6abdd3a057e4604ec4400d5b0c486f0208c4f9
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2021-12-18 13:06:57 +0100

    Add Mongod to menu tests

--- test/menu.e2e-spec.ts
+++ test/menu.e2e-spec.ts
@@ -1,21 +1,32 @@
 import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
+import { getConnectionToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Connection, Model } from 'mongoose';
+import { MenuDocument } from '../src/menu/entities/menu.entity';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { closeInMongodConnection, rootMongooseTestModule } from './helpers/MongoMemoryHelpers';
+import { MenuModule } from '../src/menu/menu.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
+  let connection: Connection
+  let menuModel: Model<MenuDocument>
 
 
   beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+    const module: TestingModule = await Test.createTestingModule({
+        imports: [
+            rootMongooseTestModule(),
+            MenuModule,
+        ]
     }).compile();
 
-    app = moduleFixture.createNestApplication();
-    app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+    connection = await module.get(getConnectionToken());
+    menuModel = connection.model('Menu')
+    app = module.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({whitelist: true}))
     await app.init();
-  });
+});
 
   describe('/menu (GET)', () => {
     it("should return 200", async () => {
@@ -35,6 +46,7 @@ describe('AppController (e2e)', () => {
 
 
   afterAll(async () => {
-    await app.close();
+    await connection.close()
+    closeInMongodConnection()
   });
 });
