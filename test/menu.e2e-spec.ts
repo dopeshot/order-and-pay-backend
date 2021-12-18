5ac40b6215f2433a4edb3e8a625c896c8dbda646
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2021-12-18 13:18:23 +0100

    Add mockdata file and in test cleanups

--- test/menu.e2e-spec.ts
+++ test/menu.e2e-spec.ts
@@ -4,49 +4,60 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { Connection, Model } from 'mongoose';
 import { MenuDocument } from '../src/menu/entities/menu.entity';
 import * as request from 'supertest';
-import { closeInMongodConnection, rootMongooseTestModule } from './helpers/MongoMemoryHelpers';
+import {
+  closeInMongodConnection,
+  rootMongooseTestModule,
+} from './helpers/MongoMemoryHelpers';
 import { MenuModule } from '../src/menu/menu.module';
+import { getTestSetupData } from './__mocks__/menuMockData';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication
-  let connection: Connection
-  let menuModel: Model<MenuDocument>
-
+  let app: INestApplication;
+  let connection: Connection;
+  let menuModel: Model<MenuDocument>;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-        imports: [
-            rootMongooseTestModule(),
-            MenuModule,
-        ]
+      imports: [rootMongooseTestModule(), MenuModule],
     }).compile();
 
     connection = await module.get(getConnectionToken());
-    menuModel = connection.model('Menu')
+    menuModel = connection.model('Menu');
     app = module.createNestApplication();
-    app.useGlobalPipes(new ValidationPipe({whitelist: true}))
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
-});
+  });
 
-  describe('/menu (GET)', () => {
-    it("should return 200", async () => {
-      await request(app.getHttpServer())
-        .get('/menu')
-        .expect(HttpStatus.OK)
-    })
-  })
+  // Insert test data
+  beforeEach(async () => {
+    await menuModel.insertMany(getTestSetupData());
+  });
 
-  describe('/menu/current (GET)', () => {
-    it("should return 204 (no active menu)", async () => {
-      await request(app.getHttpServer())
-        .get('/menu/current')
-        .expect(HttpStatus.NO_CONTENT)
-    })
-  })
-
+  // Empty the collection from all possible impurities
+  afterEach(async () => {
+    await menuModel.deleteMany();
+  });
 
   afterAll(async () => {
-    await connection.close()
-    closeInMongodConnection()
+    await connection.close();
+    closeInMongodConnection();
+  });
+  describe('/menu (GET)', () => {
+    it('should return 200', async () => {
+      await request(app.getHttpServer()).get('/menu').expect(HttpStatus.OK);
+    });
+  });
+
+  describe('/menu/current (GET)', () => {
+    it('should return 204 (no active menu)', async () => {
+      await request(app.getHttpServer())
+        .get('/menu/current')
+        .expect(HttpStatus.NO_CONTENT);
+    });
+  });
+
+  afterAll(async () => {
+    await connection.close();
+    closeInMongodConnection();
   });
 });
