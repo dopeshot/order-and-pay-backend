a741faa1dd0a729657df90a2ee85262ee534ecb6
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-01-25 11:07:19 +0100

    Fix PartialType import issue

--- src/categories/dto/update-category.dto.ts
+++ src/categories/dto/update-category.dto.ts
@@ -1,26 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
-import {
-    IsMongoId,
-    IsNotEmpty,
-    IsOptional,
-    IsString,
-    Length
-} from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateCategoryDto } from './create-category.dto';
 
-export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
-    @IsOptional()
-    @IsString()
-    @Length(2, 30)
-    title: string;
-
-    @IsOptional()
-    @IsString()
-    @Length(2, 100)
-    description: string;
-
-    @IsOptional()
-    @IsMongoId()
-    @IsNotEmpty()
-    menu: string;
-}
+export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
--- src/labels/dto/update-label.dto.ts
+++ src/labels/dto/update-label.dto.ts
@@ -1,10 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
-import { IsOptional, IsString, Length } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateLabelDto } from './create-label.dto';
-// Can't use Partial type here because class-validator doesn't support generics yet
-export class UpdateLabelDto extends PartialType(CreateLabelDto) {
-    @IsString()
-    @Length(2, 20)
-    @IsOptional()
-    title: string;
-}
+
+export class UpdateLabelDto extends PartialType(CreateLabelDto) {}
