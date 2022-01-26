7fbe75affeed403dc4f6e7986453ddda2936ef03
coffemakingtoaster <mh340@hdm-stuttgart.de>
2022-01-26 19:56:23 +0100

    add api operation to client endpoint

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -2,11 +2,12 @@ import {
     Body,
     ClassSerializerInterceptor,
     Controller,
+    HttpStatus,
     Post,
     SerializeOptions,
     UseInterceptors
 } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { MenusService } from '../menus/menus.service';
 import { CreateOrderDto } from '../orders/dtos/create-order.dto';
 import { Order } from '../orders/entities/order.entity';
@@ -24,6 +25,16 @@ export class ClientController {
 
     @Post('order')
     @ApiOperation({ summary: 'Create new order' })
+    @ApiResponse({
+        status: HttpStatus.CREATED,
+        description: 'New Order has been created',
+        type: Order
+    })
+    @ApiResponse({
+        status: HttpStatus.PAYMENT_REQUIRED,
+        description: 'Failed to create order due to payment issues ',
+        type: Order
+    })
     async create(@Body() order: CreateOrderDto) {
         const x = new Order(await this.orderService.create(order));
         return x;
