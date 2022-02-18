0d2c682d889fbe01dc31d8961dfe04b418ecc385
coffemakingtoaster <mh340@hdm-stuttgart.de>
2022-02-18 20:08:37 +0100

    add logging to guard and strategy

--- src/auth/strategies/jwt/jwt-auth.guard.ts
+++ src/auth/strategies/jwt/jwt-auth.guard.ts
@@ -1,12 +1,14 @@
 import {
     ExecutionContext,
     HttpStatus,
+    Logger,
     UnauthorizedException
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 
 export class JwtAuthGuard extends AuthGuard('jwt') {
+    private readonly logger = new Logger('JwtAuthGuard');
     public constructor(private readonly reflector: Reflector) {
         super();
     }
@@ -20,12 +22,17 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
         );
 
         // If path is public, allow access
+
         if (isPublic) {
+            this.logger.debug('A public path was called');
             return null;
         }
 
         // If user provided invalid token or was unauthorized by user validation, throw error
         if (req.authInfo || err?.status === HttpStatus.UNAUTHORIZED) {
+            this.logger.warn(
+                `User login failed. The user with id ${user.userId} tried to log in but was unauthorized`
+            );
             throw new UnauthorizedException(
                 'Your are not allowed to use this service.'
             );
@@ -33,6 +40,8 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
 
         // Currently users are not used but for the future this will sit here
         req.user = user;
+
+        this.logger.debug('A user authorized successfully');
         return user;
     }
 }
