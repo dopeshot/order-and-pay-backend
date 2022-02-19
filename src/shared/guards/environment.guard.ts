7ddc76d8bb1164ad5a390ae90f4773ef589656d3
coffemakingtoaster <mh340@hdm-stuttgart.de>
2022-02-19 16:21:16 +0100

    add method to guard logging

--- src/shared/guards/environment.guard.ts
+++ src/shared/guards/environment.guard.ts
@@ -17,14 +17,16 @@ export class ENVGuard implements CanActivate {
             this.logger.warn(
                 `A request has been blocked, due to production mode being activated. (path: ${
                     context.switchToHttp().getRequest<Request>().url
+                }, method: ${
+                    context.switchToHttp().getRequest<Request>().method
                 })`
             );
             return true;
         }
         this.logger.debug(
-            `Protected route has been called but production mode is not activated. (path: ${
+            `Protected route has been called but production mode is not activated.(path: ${
                 context.switchToHttp().getRequest<Request>().url
-            })`
+            }, method: ${context.switchToHttp().getRequest<Request>().method})`
         );
         return false;
     }
